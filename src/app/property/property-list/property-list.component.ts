b0c3b71b3fc2c1770365f5ec83097f2373d59f24
anxhelina.zhuri <anxhelinazhuri@gmail.com>
2021-04-08 22:24:10 +0200

    Add routing

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HousingService} from '../../services/housing.service';
 import {IProperty} from '../IProperty.interface';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-property-list',
@@ -8,14 +9,23 @@ import {IProperty} from '../IProperty.interface';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<IProperty> ;
-  constructor( private housingService: HousingService) { }
+  SellRent: number ;
+  properties: IProperty[] ;
+  constructor( private route: ActivatedRoute, private housingService: HousingService) { }
 
   ngOnInit(): void {
-    this.housingService.getAllProperties().subscribe(
+
+
+    if (this.route.snapshot.url.toString()) {
+      this.SellRent = 2; // Means we are on rent url , others on base
+    }
+    else {this.SellRent = 1;}
+
+    this.housingService.getAllProperties(this.SellRent).subscribe(
       data => {
       this.properties = data;
       console.log(data);
+      // console.log(this.route.snapshot.url.toString());
     }, error => {
       console.log('httperror');
       console.log(error);
