9a9b8fd0a1bd028fe27aeaf480ad7dc1ecbf2f51
anxhelina.zhuri <anxhelinazhuri@gmail.com>
2021-04-08 18:39:31 +0200

    Add add-property component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import {PropertyCardComponent} from './property/property-card/property-card.comp
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService} from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-    NavBarComponent
+    NavBarComponent,
+    AddPropertyComponent,
   ],
   imports: [
     BrowserModule,
