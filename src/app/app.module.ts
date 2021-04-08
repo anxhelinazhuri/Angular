abde24a28ac31eed0eda75e5e6524bcc5df1f46e
anxhelina.zhuri <anxhelinazhuri@gmail.com>
2021-04-08 19:57:46 +0200

    Add property detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
+import { Routes, RouterModule} from '@angular/router';
 import {PropertyCardComponent} from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService} from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
+const  appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+  { path: 'rent-property', component: PropertyListComponent },
+  { path: 'property-detail/:id', component: PropertyDetailComponent }
+
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,10 +25,13 @@ import { AddPropertyComponent } from './property/add-property/add-property.compo
     PropertyListComponent,
     NavBarComponent,
     AddPropertyComponent,
+    PropertyDetailComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [
     HousingService
