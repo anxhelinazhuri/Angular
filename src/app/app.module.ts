d4b669762acae615579c9b8b26bbfc3b74465c39
anxhelina.zhuri <anxhelinazhuri@gmail.com>
2021-04-09 15:38:08 +0200

    Added forms and validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,17 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService} from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { UserLoginComponent } from './user/user-login/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-login/user-register/user-register.component';
 
 const  appRoutes: Routes = [
   { path: '', component: PropertyListComponent },
   { path: 'add-property', component: AddPropertyComponent },
   { path: 'rent-property', component: PropertyListComponent },
   { path: 'property-detail/:id', component: PropertyDetailComponent },
+  { path: 'user/login', component: UserLoginComponent },
+  { path: 'user/register', component: UserRegisterComponent },
   { path: '**', component: PropertyListComponent }
 
 ];
@@ -26,13 +31,16 @@ const  appRoutes: Routes = [
     PropertyListComponent,
     NavBarComponent,
     AddPropertyComponent,
-    PropertyDetailComponent
+    PropertyDetailComponent,
+    UserLoginComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
-
   ],
   providers: [
     HousingService
