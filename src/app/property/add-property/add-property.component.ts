1caa837b5b90a61ca35fa4412171e4c8c95777c4
anxhelinazhuri <anxhelinazhuri@gmail.com>
2021-04-15 12:35:35 +0200

    Added control fields to Basic Info tab

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -4,6 +4,8 @@ import {FormBuilder, FormControl, FormGroup, NgForm, Validators} from '@angular/
 import {TabsetComponent, TabsModule} from 'ngx-bootstrap/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {IPropertyBase} from '../../model/ipropertybase';
+import {Property} from '../../model/property';
+import {HousingService} from '../../services/housing.service';
 
 @Component({
   selector: 'app-add-property',
@@ -16,6 +18,8 @@ export class AddPropertyComponent implements OnInit {
 
 addPropertyForm: FormGroup;
 nextClicked: boolean;
+property = new Property();
+
 propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
 furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
 propertyView: IPropertyBase = {
@@ -30,7 +34,7 @@ propertyView: IPropertyBase = {
   City: null,
   RTM: null
 };
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router, private  housingService: HousingService) { }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
@@ -49,7 +53,25 @@ propertyView: IPropertyBase = {
       }),
       PriceInfo: this.fb.group({
         Price: [null, Validators.required],
-        BuiltArea: [null, Validators.required]})
+        BuiltArea: [null, Validators.required],
+        CarpetArea: [null],
+        Security: [null],
+        Maintenance: [null],
+      }),
+      AddressInfo: this.fb.group({
+        FloorNo: [null],
+        TotalFloor: [null],
+        Address: [null, Validators.required],
+        LandMark: [null],
+      }),
+      OtherInfo: this.fb.group({
+        RTM: [null, Validators.required],
+        PossessionOn: [null],
+        AOP: [null],
+        Gated: [null],
+        MainEntrance: [null],
+        Description: [null]
+      })
     });
   }
   // ---------------------
@@ -69,31 +91,157 @@ propertyView: IPropertyBase = {
   get Price(){
     return this.PriceInfo.controls.Price as FormControl;
   }
+  get AddressInfo() {
+    return this.addPropertyForm.controls.AddressInfo as FormGroup;
+  }
+
+  get OtherInfo() {
+    return this.addPropertyForm.controls.OtherInfo as FormGroup;
+  }
+  get BHK() {
+    return this.BasicInfo.controls.BHK as FormControl;
+  }
+
+  get PType() {
+    return this.BasicInfo.controls.PType as FormControl;
+  }
+
+  get FType() {
+    return this.BasicInfo.controls.FType as FormControl;
+  }
+
+  get Name() {
+    return this.BasicInfo.controls.Name as FormControl;
+  }
+
+  get City() {
+    return this.BasicInfo.controls.City as FormControl;
+  }
+
+  get BuiltArea() {
+    return this.PriceInfo.controls.BuiltArea as FormControl;
+  }
+
+  get CarpetArea() {
+    return this.PriceInfo.controls.CarpetArea as FormControl;
+  }
+
+  get Security() {
+    return this.PriceInfo.controls.Security as FormControl;
+  }
+
+  get Maintenance() {
+    return this.PriceInfo.controls.Maintenance as FormControl;
+  }
+
+  get FloorNo() {
+    return this.AddressInfo.controls.FloorNo as FormControl;
+  }
+
+  get TotalFloor() {
+    return this.AddressInfo.controls.TotalFloor as FormControl;
+  }
+
+  get Address() {
+    return this.AddressInfo.controls.Address as FormControl;
+  }
+
+  get LandMark() {
+    return this.AddressInfo.controls.LandMark as FormControl;
+  }
+
+  get RTM() {
+    return this.OtherInfo.controls.RTM as FormControl;
+  }
+
+  get PossessionOn() {
+    return this.OtherInfo.controls.PossessionOn as FormControl;
+  }
+
+  get AOP() {
+    return this.OtherInfo.controls.AOP as FormControl;
+  }
+
+  get Gated() {
+    return this.OtherInfo.controls.Gated as FormControl;
+  }
+
+  get MainEntrance() {
+    return this.OtherInfo.controls.MainEntrance as FormControl;
+  }
+
+  get Description() {
+    return this.OtherInfo.controls.Description as FormControl;
+  }
 
   // tslint:disable-next-line:typedef
   onBack(){ this.router.navigate(['/']); }
 
   // tslint:disable-next-line:typedef
-  onSubmit( ){
+  onSubmit(){
     this.nextClicked = true;
-    if (this.BasicInfo.invalid){
-      this.formTabs.tabs[0].active = true;
-      return;
+    if (this.allTabsValid())
+    {
+      this.mapProperty();
+      this.housingService.addProperty(this.property);
+      console.log('Congrats, your property listed successfully in our website');
+      console.log(this.addPropertyForm);
     }
-    if (this.PriceInfo.invalid){
-      this.formTabs.tabs[1].active = true;
-      return;
+    else
+    {
+      console.log('Please, review the form and provide all the valid entries');
     }
-    console.log('Congrats, Form Submitted');
-    console.log('SellRent=' + this.addPropertyForm.value.BasicInfo.SellRent);
-    console.log(this.addPropertyForm);
   }
 
+  mapProperty(): void {
+    this.property.SellRent = +this.SellRent.value;
+    this.property.BHK = this.BHK.value;
+    this.property.PType = this.PType.value;
+    this.property.Name = this.Name.value;
+    this.property.City = this.City.value;
+    this.property.FType = this.FType.value;
+    this.property.Price = this.Price.value;
+    this.property.Security = this.Security.value;
+    this.property.Maintenance = this.Maintenance.value;
+    this.property.BuiltArea = this.BuiltArea.value;
+    this.property.CarpetArea = this.CarpetArea.value;
+    this.property.FloorNo = this.FloorNo.value;
+    this.property.TotalFloor = this.TotalFloor.value;
+    this.property.Address = this.Address.value;
+    this.property.Address2 = this.LandMark.value;
+    this.property.RTM = this.RTM.value;
+    this.property.AOP = this.AOP.value;
+    this.property.Gated = this.Gated.value;
+    this.property.MainEntrance = this.MainEntrance.value;
+    this.property.Possession = this.PossessionOn.value;
+    this.property.Description = this.Description.value;
+    this.property.PostedOn = new Date().toString();
+  }
   // tslint:disable-next-line:typedef
   selectTab(NextTabId: number, IsCurrentTabValid: boolean) {
     this.nextClicked = true;
     if (IsCurrentTabValid){
     this.formTabs.tabs[NextTabId].active = true;
     }
+  }
+
+  allTabsValid(): boolean {
+    if (this.BasicInfo.invalid){
+        this.formTabs.tabs[0].active = true;
+        return false ;
+    }
+    if (this.PriceInfo.invalid){
+      this.formTabs.tabs[1].active = true;
+      return false ;
+    }
+    if (this.AddressInfo.invalid) {
+      this.formTabs.tabs[2].active = true;
+      return false;
+    }
+    if (this.OtherInfo.invalid) {
+      this.formTabs.tabs[3].active = true;
+      return false;
+    }
+    return true;
   }
 }
